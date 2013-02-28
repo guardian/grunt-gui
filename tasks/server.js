ac0d81c22c940878f36b8aec7adb96bb2606066f
Rich Harris <richard.a.harris@gmail.com>
2013-02-28 15:52:33 +0000

    changed server task interface and behaviour

--- tasks/server.js
+++ tasks/server.js
@@ -6,36 +6,53 @@ module.exports = function ( grunt ) {
 
 	grunt.registerMultiTask( 'server', 'Launch a preview server', function () {
 
-		var done, options, app, connect, middleware;
+		var done, options, app, connect, mime, middleware;
 
 		done = this.async();
 
 		connect = require( 'connect' );
+		mime = require( 'mime' );
 
 		options = this.options();
 		middleware = [];
 
-		// dynamic responses
-		if ( options.dynamic ) {
-			options.dynamic.forEach( function ( item ) {
-				middleware[ middleware.length ] = function ( req, res, next ) {
-					if ( item.filter( req ) ) {
-						res.end( item.response() );
+		options.mappings.forEach( function ( mapping ) {
+			middleware[ middleware.length ] = function ( req, res, next ) {
+				var prefix, mimetype, data, src, i, complete, folder, relpath, filepath;
+
+				prefix = mapping.prefix;
+
+				// if the request URL matches the prefix...
+				if ( req.url.indexOf( prefix ) === 0 ) {
+					// ... try each of the src folders in turn
+					src = ( typeof mapping.src === 'string' ? [ mapping.src ] : mapping.src );
+
+					for ( i=0; i<src.length; i+=1 ) {
+						folder = src[i];
+
+						relpath = req.url.substring( prefix.length );
+						filepath = folder + relpath;
+
+						if ( grunt.file.exists( filepath ) ) {
+							mimetype = mime.lookup( filepath );
+
+							res.setHeader( 'Content-Type', mimetype );
+							res.end( grunt.file.read( filepath ) );
+							complete = true;
+							break;
+						}
 					}
 
-					else {
+					if ( !complete ) {
 						next();
 					}
-				};
-			});
-		}
+				}
 
-		// static responses
-		if ( options.static ) {
-			options.static.forEach( function ( root ) {
-				middleware[ middleware.length ] = connect.static( root );
-			});
-		}
+				else {
+					next();
+				}
+			};
+		});
 
 		app = connect();
 
@@ -43,7 +60,14 @@ module.exports = function ( grunt ) {
 			app = app.use( middleware );
 		});
 
+		app.use( function ( req, res, next ) {
+			res.statusCode = 404;
+			res.end( 'File not found: ' + req.url + ' (have you run grunt?)' );
+		});
+
 		app.listen( options.port );
+
+		grunt.log.writeln( 'Listening on port ' + options.port + '. Hit Ctrl+C to kill server' );
 
 	});
 
