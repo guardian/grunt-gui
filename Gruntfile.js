429bcd3a38ec6053b3b5e5ac7121538c6570c99b
Rich Harris <richard.a.harris@gmail.com>
2013-03-05 22:11:04 +0000

    removed gubbins

--- Gruntfile.js
+++ Gruntfile.js
@@ -154,7 +154,6 @@ module.exports = function(grunt) {
 	// These plugins provide necessary tasks.
 	grunt.loadNpmTasks('grunt-contrib-jshint');
 	grunt.loadNpmTasks('grunt-contrib-clean');
-	grunt.loadNpmTasks('grunt-contrib-nodeunit');
 	grunt.loadNpmTasks('grunt-shell');
 
 
